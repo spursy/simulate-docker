162d70a067197c7c65fe72a97bd6a6f2463be3ec
yang yang <yang.yang@5th.work>
2019-03-11 19:08:46 +0800

    update main.go file

--- uts/main.go
+++ uts/main.go
@@ -1,26 +1,3 @@
-// package main
-
-// import (
-// 	"os/exec"
-// 	"syscall"
-// 	"os"
-// 	"log"
-// )
-
-// func main() {
-// 	cmd := exec.Command("sh")
-// 	cmd.SysProcAttr = &syscall.SysProcAttr{
-// 		Cloneflags: syscall.CLONE_NEWUTS,
-// 	}
-// 	cmd.Stdin = os.Stdin
-// 	cmd.Stdout = os.Stdout
-// 	cmd.Stderr = os.Stderr
-
-// 	if err := cmd.Run(); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
 package main
 import (
     "os/exec"
